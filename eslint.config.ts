8557a3f2c10c0ed62cbc4cb612957c2273598b4d
renee <50965960+wurenee@users.noreply.github.com>
2025-08-08 14:27:36 -0700

    add lint security

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
   {
@@ -15,6 +16,6 @@ export default defineConfig([
   },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
-  
+  pluginSecurity.configs.recommended,
 ]);
 
